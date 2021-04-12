99cb475488e2f0659a8d74b858604b9d501e3f29
Andrey2980 <aazaroff55@gmail.com>
2021-04-12 12:21:08 +0300

    Урок 3 поправил + урок 4

---
+++ work4.js
@@ -0,0 +1,44 @@
+//заданме номер 1
+
+function decompositionOfNumber() {
+    let num = +prompt("Введите число");
+    let digit = 0;
+
+    let number = {
+        units: "",
+        dozens: "",
+        hundreds: "",
+    };
+
+    let array = [0, 0, 0];
+
+    if (num > 999)
+        return number;
+    
+    for (let i = 0; i < array.length - 1; i++) {
+        digit = num % 10;
+        array[i] = digit;
+        num = Math.floor(num / 10);
+    }
+
+    number.units = array[0];
+    number.dozens = array[1];
+    number.hundreds = num;
+
+    return number;
+}
+
+console.log(decompositionOfNumber());
+
+
+
+//задание номер 1
+
+let korobka = {
+    id: 00001,
+    name: "apple",
+    price: 1123.09,
+    description: "<P>"
+}
+
+console.log(korobka.id + korobka.price); //почему такой результат 1124.09 ?
